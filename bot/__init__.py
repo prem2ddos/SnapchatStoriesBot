a99041f739ac257f8281451e5973652c0870a7a0
prem2ddos <habibisekh859@gmail.com>
2025-05-25 22:47:30 +0530

    __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -10,11 +10,11 @@ logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 
 class ENV_VARS(object):
-    API_ID = int(os.environ.get("API_ID", "22151179"))
-    API_HASH = os.environ.get("API_HASH", "d0796594c68e193879899b56220a7502")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7803927725:AAFTMz9ddegKN3UfUkNfY4UVbM5Gs6CZn2I")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "SMHACKERS_bot")
-    #AUTH_USER = int(os.environ.get("AUTH_USER", 5071059420))
+    API_ID = int(os.environ.get("API_ID", "25109257"))
+    API_HASH = os.environ.get("API_HASH", "ca6e12ee79189ad6a5d01e6f3a2f9b31")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7800823134:AAEyNMRxufF-cEhamAD2jBo3mt7jfp8TTRo")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "snakesnapbot")
+    #AUTH_USER = int(os.environ.get("AUTH_USER", 7820924114))
 
 
 Config = ENV_VARS
